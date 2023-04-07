4b6accba78c383ae4387a166f1ac12a5c6f4c41c
tedi <tediago03@gmail.com>
2023-04-07 17:20:41 +0200

    refactoring and development

--- src/main/java/ta/presentation/dentalt/DentaltApplication.java
+++ src/main/java/ta/presentation/dentalt/DentaltApplication.java
@@ -2,8 +2,9 @@ package ta.presentation.dentalt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class DentaltApplication {
 
 	public static void main(String[] args) {
